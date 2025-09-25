1d8086ab6ade099f11c4403dec3b0a1fe6d57b6c
hdaibxxj <3501592418@qq.com>
2025-09-25 16:39:10 -0400

    Add files via upload

---
+++ src/main/java/com/clean/game/MainActivity.java
@@ -0,0 +1,22 @@
+package com.clean.game;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.webkit.WebView;
+import android.webkit.WebSettings;
+
+public class MainActivity extends Activity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        WebView webView = new WebView(this);
+        WebSettings settings = webView.getSettings();
+        settings.setJavaScriptEnabled(true);
+        settings.setDomStorageEnabled(true);
+        settings.setAllowFileAccess(true);
+        
+        webView.loadUrl("file:///android_asset/www/index.html");
+        setContentView(webView);
+    }
+}
